052b4120bb93e988d7da7c9e936c7eeae78e1c09
Ekta-pawar <ektapawar0001@gmail.com>
2025-07-27 11:41:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+const board = document.getElementById('board');
+
+for (let i = 0; i < 800; i++) {
+  const box = document.createElement('div');
+  box.className = 'box';
+
+  box.addEventListener("mouseover", () => {
+    box.style.backgroundColor = colorGenerator();
+  });
+
+  box.addEventListener("mouseout", () => {
+    box.style.backgroundColor = "#444"; // reset color
+  });
+
+  board.appendChild(box);
+}
+
+function colorGenerator() {
+  const colors = ["#e74c3c", "#8e44ad", "#3498db", "#1abc9c", "#f39c12", "#2ecc71"];
+return colors[Math.floor(Math.random()*colors.length)]
+}
